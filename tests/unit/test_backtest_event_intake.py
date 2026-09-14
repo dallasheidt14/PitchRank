4916a8dbdc184e73a87deb1e1fa70c6b4fcee143
dallasheidt14 <dallasheidt@gmail.com>
2026-09-13 18:12:10 -0700

    test(matchbalance): cover historical report dependency

--- tests/unit/test_backtest_event_intake.py
+++ tests/unit/test_backtest_event_intake.py
@@ -153,6 +153,7 @@ _READ_ONLY_MODULES = (
     "src/tournaments/backtest_event_intake.py",
     "src/tournaments/backtest_event_rollup.py",
     "src/tournaments/backtest_historical_preflight.py",
+    "src/tournaments/historical_coverage_reporting.py",
     "src/tournaments/backtest_intake_state.py",
     "src/tournaments/backtest_intake_ui.py",
     "src/tournaments/backtest_link_store.py",
